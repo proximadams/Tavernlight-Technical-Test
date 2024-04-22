798a085fe3dd141e5ccc8c5b4b831542027d4382
Lucky Adams <proximadams@gmail.com>
2024-04-22 11:14:21 -0500

    q1 defensive programming/error throwing

--- q1.lua
+++ q1.lua
@@ -1,14 +1,16 @@
--- are setStorageValue and getStorageValue TFS & OTC only?
--- onLogout() returns true before releaseStorage() is actually called. addEvent has delay of 1000. Yield/await maybe?
-
 local <const> DELAY_MS = 1000
 local <const> PLAYER_INDEX = 1000
 
+local isValidPlayer(player)
+    return (player and type(player.setStorageValue) == 'function' and type(player.getStorageValue) == 'function')
+
 local function releaseStorage(player)
+    assert(isValidPlayer(player), 'Invalid player. Does not have getter and setter storage functions')
     player:setStorageValue(PLAYER_INDEX, -1)
 end
 
 function onLogout(player)
+    assert(isValidPlayer(player), 'Invalid player. Does not have getter and setter storage functions')
     if player:getStorageValue(PLAYER_INDEX) == 1 then
         addEvent(releaseStorage, DELAY_MS, player)
     end
