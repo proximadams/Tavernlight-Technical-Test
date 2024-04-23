97f8471c0bb101915290a17176b7f575ae1cd8c6
Lucky Adams <proximadams@gmail.com>
2024-04-22 20:36:39 -0500

    fix not equal

--- q2.lua
+++ q2.lua
@@ -4,7 +4,7 @@ function printSmallGuildNames(memberCount)
     local selectGuildQuery = "SELECT name FROM guilds WHERE max_members < %d;"
     local resultId = db.storeQuery(string.format(selectGuildQuery, memberCount))
     -- prints all guilds, not just the first one
-    if resultId != nil then
+    if resultId ~= nil then
         local guildName = result.getString(resultId, "name")
         while guildName do
             print(guildName)
