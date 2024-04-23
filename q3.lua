3f0ceed075753ec213a14f14b705bee6bf61f8f0
Lucky Adams <proximadams@gmail.com>
2024-04-22 21:14:21 -0500

    q3 comments

--- q3.lua
+++ q3.lua
@@ -2,8 +2,9 @@
 function removeMemberFromParty(playerId, membername)
     local player = Player(playerId)
     local party = player:getParty()
-    local memberToRemove = Player(membername)
+    local memberToRemove = Player(membername)-- Avoids running Player(membername) code twice
 
+    -- k and v variable names are not very descriptive so I changed them
     for memberKey,memberValue in pairs(party:getMembers()) do
         if memberValue == memberToRemove then
             party:removeMember(memberToRemove)
