068b41b176515855e74cc820e7ade6a743350953
Lucky Adams <proximadams@gmail.com>
2024-04-22 20:31:48 -0500

    replace single quotes with double quotes

--- q1.lua
+++ q1.lua
@@ -2,15 +2,15 @@ local <const> DELAY_MS = 1000
 local <const> PLAYER_INDEX = 1000
 
 local isValidPlayer(player)
-    return (player and type(player.setStorageValue) == 'function' and type(player.getStorageValue) == 'function')
+    return (player and type(player.setStorageValue) == "function" and type(player.getStorageValue) == "function")
 
 local function releaseStorage(player)
-    assert(isValidPlayer(player), 'Invalid player. Does not have getter and setter storage functions')
+    assert(isValidPlayer(player), "Invalid player. Does not have getter and setter storage functions")
     player:setStorageValue(PLAYER_INDEX, -1)
 end
 
 function onLogout(player)
-    assert(isValidPlayer(player), 'Invalid player. Does not have getter and setter storage functions')
+    assert(isValidPlayer(player), "Invalid player. Does not have getter and setter storage functions")
     if player:getStorageValue(PLAYER_INDEX) == 1 then
         addEvent(releaseStorage, DELAY_MS, player)
     end
