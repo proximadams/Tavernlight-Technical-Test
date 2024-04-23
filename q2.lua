4445cc0bfa82e92413da1e48b1141f0210fe4c03
Lucky Adams <proximadams@gmail.com>
2024-04-22 20:35:22 -0500

    Q2 fix call getString with resultId & print all guilds

--- q2.lua
+++ q2.lua
@@ -1,9 +1,17 @@
--- db.storeQuery TFS & OTC only
 
 function printSmallGuildNames(memberCount)
     -- this method is supposed to print names of all guilds that have less than memberCount max members
-    local selectGuildQuery = "SELECT name FROM guilds WHERE max_members < %d;" -- need single quotes around 'name' and 'guilds' maybe?
-    local resultId = db.storeQuery(string.format(selectGuildQuery, memberCount)) -- string.format safe from injection?
-    local guildName = result.getString("name")
-    print(guildName)
+    local selectGuildQuery = "SELECT name FROM guilds WHERE max_members < %d;"
+    local resultId = db.storeQuery(string.format(selectGuildQuery, memberCount))
+    -- prints all guilds, not just the first one
+    if resultId != nil then
+        local guildName = result.getString(resultId, "name")
+        while guildName do
+            print(guildName)
+            if not result.next(resultId) then
+                break
+            end
+            guildName = result.getString(resultId, "name")
+        end
+    end
 end
