46ebc4afb039a5007c27c209a713c0a7ac25d26b
Lucky Adams <proximadams@gmail.com>
2024-04-22 20:48:21 -0500

    renamed q3 function and minor refactoring

--- q3.lua
+++ q3.lua
@@ -1,11 +1,12 @@
--- function remove_player_from_party(...) ?
-function do_sth_with_PlayerParty(playerId, membername)
-    player = Player(playerId)
-    local party = player:getParty()
 
-    for k,v in pairs(party:getMembers()) do
-        if v == Player(membername) then-- change 'Player(membername)' to 'player'?
-            party:removeMember(Player(membername))-- change 'Player(membername)' to 'player'?
+function remove_member_from_party(playerId, membername)
+    local player = Player(playerId)
+    local party = player:getParty()
+    local memberToRemove = Player(membername)
+
+    for memberKey,memberValue in pairs(party:getMembers()) do
+        if memberValue == memberToRemove then
+            party:removeMember(memberToRemove)
         end
     end
 end
