6a2a323fb8fec3333ae53a7f4e9f96843b839d48
Lucky Adams <proximadams@gmail.com>
2024-04-22 21:07:04 -0500

    refactor q1

--- q1.lua
+++ q1.lua
@@ -1,16 +1,17 @@
 local <const> DELAY_MS = 1000
 local <const> PLAYER_INDEX = 1000
 
-local isValidPlayer(player)
-    return (player and type(player.setStorageValue) == "function" and type(player.getStorageValue) == "function")
+local validatePlayer(player)
+    local isValid = (player and type(player.setStorageValue) == "function" and type(player.getStorageValue) == "function")
+    assert(isValid, "Invalid player. Does not have getter and setter storage functions")
 
 local function releaseStorage(player)
-    assert(isValidPlayer(player), "Invalid player. Does not have getter and setter storage functions")
+    validatePlayer(player)
     player:setStorageValue(PLAYER_INDEX, -1)
 end
 
 function onLogout(player)
-    assert(isValidPlayer(player), "Invalid player. Does not have getter and setter storage functions")
+    validatePlayer(player)
     if player:getStorageValue(PLAYER_INDEX) == 1 then
         addEvent(releaseStorage, DELAY_MS, player)
     end
